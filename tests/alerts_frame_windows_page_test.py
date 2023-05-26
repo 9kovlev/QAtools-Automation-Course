901bf5040af39ccb5bca1ac844deb1540183af76
Slava Yakovlev <dddjemy@gmail.com>
2023-05-26 22:38:10 +0300

    Updated alerts_frame_windows_page_locators,alerts_frame_windows_page, alerts_frame_windows_page_test
    added class:
    TestAlertsPage
    
    added tests:
    test_see_alert
    test_alert_appear_5_sec
    test_confirm_alert
    test_prompt_alert

--- tests/alerts_frame_windows_page_test.py
+++ tests/alerts_frame_windows_page_test.py
@@ -1,8 +1,7 @@
-from pages.alerts_frame_windows_page import BrowserWindowsPage
+from pages.alerts_frame_windows_page import BrowserWindowsPage, AlertsPage
 
 
 class TestAlertsFrameWindow:
-
     class TestBrowserWindow:
         def test_new_tab(self, driver):
             browser_window_page = BrowserWindowsPage(driver, "https://demoqa.com/browser-windows")
@@ -16,3 +15,28 @@ class TestAlertsFrameWindow:
             result = browser_window_page.check_opened_new_tab()
             assert result == 'This is a sample page', "The new tab has not been opened "
 
+    class TestAlertsPage:
+
+        def test_see_alert(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            alert_text = alert_page.check_see_alert()
+            assert alert_text == 'You clicked a button', "Alert did not show up"
+
+        def test_alert_appear_5_sec(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            alert_text = alert_page.check_alert_appear_5_sec()
+            assert alert_text == 'This alert appeared after 5 seconds', "Alert did not show up"
+
+        def test_confirm_alert(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            alert_text = alert_page.check_confirm_alert()
+            assert alert_text == 'You selected Ok', "Alert did not show up"
+
+        def test_prompt_alert(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            text, alert_text = alert_page.check_prompt_alert()
+            assert text in alert_text, "Alert did not show up"
